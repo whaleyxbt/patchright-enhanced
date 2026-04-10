59bccc18220ec91865499134b87a5c4c1222de3a
whaley <165095936+whaleyxbt@users.noreply.github.com>
2026-04-10 11:54:45 +0300

    locale disable

--- src/config/browser.config.ts
+++ src/config/browser.config.ts
@@ -4,7 +4,7 @@ export function getBrowserConfig(): BrowserConfig {
   return {
     headless: false,
     executablePath: process.env.BROWSER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable',
-    locale: process.env.BROWSER_LOCALE || 'de-DE',
+//    locale: process.env.BROWSER_LOCALE || 'de-DE',
     timezoneId: process.env.BROWSER_TIMEZONE || 'Europe/Berlin',
   };
 }
